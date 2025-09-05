316e8378579bfa0055b4e25422b13d6a6c70044e
Tanay Mehendale <tanaymehendale@gmail.com>
2025-09-04 18:24:40 -0700

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProcessComponent } from './process/process.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'process', component: ProcessComponent},
+  { path: 'about', component: AboutComponent},
+
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
